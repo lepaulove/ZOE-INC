24696e700027f44c1167ffbebd7c9548095a6ad1
lepaulove <lepaul.love@gmail.com>
2022-10-04 14:08:13 -0500

    Updates

--- src/Firebase/config.js
+++
@@ -1,9 +0,0 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-    apiKey: "AIzaSyCb17pjM3DSH2Jcz2l_gRTrYO1iaWnWcdY",
-    authDomain: "zoe-inc-and-associates.firebaseapp.com",
-    projectId: "zoe-inc-and-associates",
-    storageBucket: "zoe-inc-and-associates.appspot.com",
-    messagingSenderId: "41976103659",
-    appId: "1:41976103659:web:33ba6d1bf5643c0d3474ae"
-  };
