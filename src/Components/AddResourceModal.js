84cb1050cf49011f36fa112124ee7a146124e0b5
lepaulove <lepaul.love@gmail.com>
2022-10-22 16:57:12 -0500

    complete modal set up to allow admin to add resources from admin page. Still need to send reousrce info to backend(firebase)

--- src/Components/AddResourceModal.js
+++ src/Components/AddResourceModal.js
@@ -10,9 +10,14 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { TextField, Grid } from '@mui/material';
+import styled from 'styled-components';
+import { TextField, Grid, List, ListItem } from '@mui/material';
 import { firestore } from '../Firebase/utils';
 
+const BootstrapButton = styled(Button)({
+  backgroundColor: '#000',
+  marginLeft: 40
+})
 
 export default function AddResourceModal(props) {
     const { open, onClose } = props;
@@ -22,38 +27,62 @@ export default function AddResourceModal(props) {
     const [contactType, setContactType] = useState('')
     const [contactInfo, setContactInfo] = useState('')
     const [details, setDetails] = useState('')
-//   const [open, setOpen] = React.useState(false);
+    const [selectedContactTypes, setSelectedContactTypes] = useState([])
+    const contactTypes = ['Hotline', 'Warm Line', 'Chat', 'Website', 'Text', 'App', 'Online Forum', 'Online Meetings', 'Online Support Groups', 'Local Meetings', 'Help Line']
+    const categories = ['Crisis & Warm Support', 'Covid-19', 'Domestic Violence', 'Sexual Assault', 'LGBTQ+', 'Substance Abuse', 'Veterans', 'Families', 'EDOs', 'Necessities', 'Other']
 
-//   const handleClickOpen = () => {
-//     setOpen(true);
-//   };
+    const handleCategoryChange = e => {
+      setCategory(e.target.value)
+    }
 
-    // const [userID, setUserId] = useState('')
-    // const [displayName, setDisplayName] = useState('')
+    const handleContactTypeChange = e => {
+      setContactType(e.target.value)
+    }
 
-    // useEffect(() => {
-    //   setUserId(user.id)
-    //   setDisplayName(user.data().displayName)
-    // }, [])
+    const handleContactInfoChange = e => {
+      setContactInfo(e.target.value)      
+    }
 
+    const handleDetailsChange = e => {
+      setDetails(e.target.value)      
+    }
 
+    const handleFocusChange = e => {
+      setFocus(e.target.value)      
+    }
+
+    const resetState = () => {
+      setCategory('')
+      setFocus('')
+      setContactType('')
+      setContactInfo('')
+      setDetails('')
+      setSelectedContactTypes([])
+    }
+
+    const handleAddContactInfo = () => {
+      setSelectedContactTypes([...selectedContactTypes, {contactType: contactType, contactInfo: contactInfo}])
+      setContactType('')
+      setContactInfo('')
+      console.log(selectedContactTypes)
+    }
+  
   const handleSendClose = () => {
 //     // if(user)
-//     deleteUser(selectedValue.id) 
+//     deleteUser(selectedValue.id)
+    console.log(`Category: ${category}\nFocus: ${focus}\nContact Type: ${contactType}\nContact Info: ${contactInfo}\nDetails: ${details}\n`)
+    resetState()
     
     onClose()
   };
 
   const handleClose = () => {
+    resetState()
     onClose()
   };
 
   return (
-    <div>
-      {/* <Button variant="outlined" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button> */}
-      {/* {props.theButton} */}
+    <>
       <Dialog
         open={open}
         onClose={handleClose}
@@ -68,28 +97,64 @@ export default function AddResourceModal(props) {
             <Grid container rowSpacing={1.5} sx={{mt:1}}>
                 <Grid container item spacing={1.5}>
                     <Grid item xs={6}>
-                        <TextField label='Category' fullWidth/>
+                    <Box sx={{ minWidth: 120 }}>
+                          <FormControl fullWidth>
+                            <InputLabel id="simple-select-label">Category</InputLabel>
+                            <Select
+                              labelId="simple-select-label"
+                              id="simple-select"
+                              value={category}
+                              label="Contact Type"
+                              onChange={handleCategoryChange}
+                            >{categories.map((item, index) => {
+                              return (<MenuItem key={index} value={item}>{item}</MenuItem>)
+                            })}
+                            </Select>
+                          </FormControl>
+                        </Box>
                     </Grid>
                     <Grid item xs={6}>
-                        <TextField label='Focus' fullWidth/>
+                        <TextField label='Focus' fullWidth value={focus} onChange={handleFocusChange}/>
                     </Grid>
                     <Grid item container rowSpacing={1.5}>
                       <Grid item xs={12}>
-                          <BasicSelect />
+                        <Box sx={{ minWidth: 120 }}>
+                          <FormControl fullWidth>
+                            <InputLabel id="simple-select-label">Contact Type</InputLabel>
+                            <Select
+                              labelId="simple-select-label"
+                              id="simple-select"
+                              value={contactType}
+                              label="Contact Type"
+                              onChange={handleContactTypeChange}
+                            >{contactTypes.map((item, index) => {
+                              return (<MenuItem key={index} value={item}>{item}</MenuItem>)
+                            })}
+                            </Select>
+                          </FormControl>
+                        </Box>
                       </Grid>
                       <Grid item xs={12}>
-                          <TextField label='Contact Info' fullWidth/>
+                          <TextField label='Contact Info' fullWidth value={contactInfo} onChange={handleContactInfoChange}/>
                       </Grid>
+                      <BootstrapButton variant='contained' onClick={() => handleAddContactInfo()}>Add Contact Info</BootstrapButton>
                     </Grid>
                 </Grid>
                 <Grid container item>
                     <Grid item xs={12}>
-                        <TextField label='Details' fullWidth multiline minRows={4}/>
+                        <TextField label='Details' fullWidth multiline minRows={4} value={details} onChange={handleDetailsChange}/>
+                    </Grid>
+                </Grid>
+                <Grid container item>
+                    <Grid item xs={12}>
+                        <List>
+                          {selectedContactTypes.map((item, index)=> {
+                            return <ListItem key={index}>{` - Type: ${item.contactType} Info: ${item.contactInfo}`}</ListItem>
+                          })}
+                        </List>
                     </Grid>
                 </Grid>
             </Grid>
-            
-            {/* {`Permanently Delete ${selectedValue.data().displayName}?`} */}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
@@ -97,7 +162,7 @@ export default function AddResourceModal(props) {
           <Button onClick={handleClose} autoFocus>Cancel</Button>
         </DialogActions>
       </Dialog>
-    </div>
+    </>
   );
 }
 
@@ -106,7 +171,7 @@ function BasicSelect() {
     const contactTypes = ['Hotline', 'Chat', 'Website', 'Text', 'App', 'Online Forum', 'Online Meetings', 'Local Meetings', 'Help Line']
   
     const handleChange = (e) => {
-      setContactType(e);
+      setContactType(e.target.value);
     };
   
     return (
